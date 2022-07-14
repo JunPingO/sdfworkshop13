67b9b1203e07d1868b1e9c2f932fdf261f748b87
JunPingO <ongjunping@gmail.com>
2022-07-14 12:06:54 +0800

    first

---
+++ src/main/java/vttp/workshop13/sdfworkshop13/Sdfworkshop13Application.java
@@ -0,0 +1,14 @@
+package vttp.workshop13.sdfworkshop13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdfworkshop13Application {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Sdfworkshop13Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/workshop13/sdfworkshop13/Sdfworkshop13ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.workshop13.sdfworkshop13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdfworkshop13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
